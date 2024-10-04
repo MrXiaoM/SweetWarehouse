afdc04061cd33f9191023bbf4df33235f84254af
人間工作 <coolxiaom95@gmail.com>
2024-10-04 15:46:18 +0800

    cache

--- src/main/java/top/mrxiaom/sweet/warehouse/database/ItemsDatabase.java
+++ src/main/java/top/mrxiaom/sweet/warehouse/database/ItemsDatabase.java
@@ -1,17 +1,31 @@
 package top.mrxiaom.sweet.warehouse.database;
 
+import com.google.common.collect.Lists;
+import com.google.common.io.ByteArrayDataOutput;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import top.mrxiaom.pluginbase.database.IDatabase;
+import top.mrxiaom.pluginbase.utils.Bytes;
 import top.mrxiaom.sweet.warehouse.SweetWarehouse;
 import top.mrxiaom.sweet.warehouse.func.AbstractPluginHolder;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class ItemsDatabase extends AbstractPluginHolder implements IDatabase {
+public class ItemsDatabase extends AbstractPluginHolder implements IDatabase, Listener {
     public static class Item {
         public final String item;
         public final int amount;
@@ -22,8 +36,62 @@ public class ItemsDatabase extends AbstractPluginHolder implements IDatabase {
         }
     }
     private String TABLE_NAME;
+    private final Map<String, List<Item>> cache = new ConcurrentHashMap<>();
     public ItemsDatabase(SweetWarehouse plugin) {
-        super(plugin);
+        super(plugin, true);
+        registerEvents();
+        registerBungee();
+    }
+
+    public void broadcastRemoveCache(String player) {
+        List<Player> players = Lists.newArrayList(Bukkit.getOnlinePlayers());
+        Player p = players.isEmpty() ? null : players.get(0);
+        if (p != null) {
+            ByteArrayDataOutput output = Bytes.newDataOutput();
+            output.writeUTF("Forward");
+            output.writeUTF("ALL");
+            output.writeUTF("SweetWarehouse");
+            try (ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                 DataOutputStream out = new DataOutputStream(bytes)) {
+                out.writeInt(0);
+                out.writeUTF(player.toLowerCase());
+                output.writeShort(bytes.toByteArray().length);
+                output.write(bytes.toByteArray());
+            } catch (IOException e) {
+                warn(e);
+                return;
+            }
+            p.sendPluginMessage(plugin, "BungeeCord", output.toByteArray());
+        }
+    }
+
+    @Override
+    public void receiveBungee(String subChannel, DataInputStream in) throws IOException {
+        if (subChannel.equals("SweetWarehouse")) {
+            int type = in.readInt();
+            if (type == 0) {
+                cache.remove(in.readUTF());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(AsyncPlayerPreLoginEvent e) {
+        cache.remove(e.getName().toLowerCase());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        onPlayerLeave(e.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent e) {
+        onPlayerLeave(e.getPlayer());
+    }
+
+    private void onPlayerLeave(Player player) {
+        cache.remove(player.getName().toLowerCase());
     }
 
     @Override
@@ -46,6 +114,8 @@ public class ItemsDatabase extends AbstractPluginHolder implements IDatabase {
     }
 
     public List<Item> getItems(String player) {
+        List<Item> cacheItems = cache.get(player.toLowerCase());
+        if (cacheItems != null) return cacheItems;
         try (Connection conn = plugin.getConnection();
              PreparedStatement ps = conn.prepareStatement(
                      "SELECT * FROM " + TABLE_NAME + " WHERE `player`=?"
@@ -77,6 +147,7 @@ public class ItemsDatabase extends AbstractPluginHolder implements IDatabase {
                      ? ("INSERT OR REPLACE INTO " + TABLE_NAME + "(`player`,`item`,`amount`) VALUES(?, ?, ?)")
                      : ("INSERT INTO " + TABLE_NAME + "(`player`,`item`,`amount`) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE `amount`=?"))
         ) {
+            broadcastRemoveCache(player);
             psGet.setString(1, player);
             psGet.setString(2, item);
             ResultSet resultGet = psGet.executeQuery();
@@ -99,6 +170,7 @@ public class ItemsDatabase extends AbstractPluginHolder implements IDatabase {
                      "SELECT * FROM " + TABLE_NAME + " WHERE `player`=? AND `item`=?"
              );
         ) {
+            broadcastRemoveCache(player);
             psGet.setString(1, player);
             psGet.setString(2, item);
             ResultSet resultGet = psGet.executeQuery();
